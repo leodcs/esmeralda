b7951abe79aa5ef332e1d682b04768707e9a10f0
Leonardo Santos <aleotory@gmail.com>
2020-10-02 15:14:28 -0300

    Corrige script principal para compilação com OCRA

--- compiler.rb
+++ compiler.rb
@@ -1,5 +1,4 @@
 Dir['./lib/config/**/*.rb'].sort.each { |config| require config }
-
 require 'colorize'
 require 'tty-reader'
 require 'yaml/store'
@@ -11,6 +10,12 @@ require './lib/nodes/node'
 require './lib/nodes/expression'
 Dir['./lib/exceptions/*.rb'].each { |exception| require exception }
 Dir['./lib/nodes/*.rb'].sort.each { |node| require node }
+
+# Configura pasta atual para ser a mesma do executável
+if ENV['OCRA_EXECUTABLE']
+  pasta = File.dirname(ENV['OCRA_EXECUTABLE'])
+  Dir.chdir(pasta)
+end
 
 class Compiler
   def compile(file)
@@ -32,11 +37,10 @@ class Compiler
   end
 end
 
-caminho_executavel = ENV.fetch('OCRA_EXECUTABLE', Dir.pwd)
 reader = TTY::Reader.new
 
 reader.on(:keyescape) do
-  puts "Finalizando..."
+  puts 'Finalizando...'
   exit
 end
 
@@ -44,21 +48,22 @@ loop do
   puts 'Tecle ESC para encerrar ou digite o nome do arquivo e tecle Enter'
   nome_arquivo = reader.read_line('Nome do arquivo (sem extensão): ').chomp
   next if nome_arquivo.blank?
-  caminho_arquivo = File.join('.', nome_arquivo)
 
-  if File.file?(caminho_arquivo)
-    arquivo = File.read(caminho_arquivo)
+  if File.file?(nome_arquivo)
+    arquivo = File.read(nome_arquivo)
     begin
       puts 'Compilando...'.colorize(:blue)
+
       Compiler.new.compile(arquivo)
+
       puts 'Compilação efetuada com sucesso.'.colorize(:green)
-    rescue StandardError => exception
-      exception.set_backtrace([])
-      puts exception.message.colorize(:red)
+    rescue StandardError => erro_esperado
+      erro_esperado.set_backtrace([])
+      puts erro_esperado.message.colorize(:red)
     ensure
       puts
     end
   else
-    puts "Arquivo não encontrado: \"#{nome_arquivo}\"".colorize(:red)
+    puts "Arquivo #{nome_arquivo.inspect} não encontrado na pasta #{Dir.pwd.inspect}".colorize(:red)
   end
 end
