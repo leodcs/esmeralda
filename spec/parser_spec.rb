7c58e1b4d16ccd8a5587b783a152d475390bb736
Leonardo Santos <aleotory@gmail.com>
2020-10-08 21:29:41 -0300

    Adiciona testes automatizados para Parser

---
+++ spec/parser_spec.rb
@@ -0,0 +1,47 @@
+require '../lib/scanner'
+require '../lib/parser'
+
+RSpec.describe Parser do
+  it 'Verifica ordem dos tokens' do
+    arquivo = File.read('spec/algoritmos/algoritmo1')
+    $scan = Scanner.new(arquivo).scan
+    parse = Parser.new.parse
+
+    expect(parse.class).to eq(Parser)
+    expect(parse.declarations.count).to eq(14)
+    expect(parse.assignments.count).to eq(14)
+    expect(parse.identifiers.count).to eq(28)
+    expect(parse.calls.count).to eq(2)
+  end
+
+  it 'Detecta símbolos inesperados' do
+    files = [
+      ['erro2_comentario', 'ERRO 02: Símbolo "}" inesperado. Esperando END - Linha 9, Coluna 9.'],
+      ['erro2_programa', 'ERRO 02: Símbolo "PROGRAMA" inesperado. Esperando PROGRAM - Linha 1, Coluna 1.'],
+      ['erro2_bloco_principal', 'ERRO 02: Símbolo "END" inesperado. Esperando BEGIN - Linha 3, Coluna 1.'],
+      ['erro2_decl_var', 'ERRO 02: Símbolo ";" inesperado. Esperando ID - Linha 2, Coluna 7.'],
+      ['erro2_tipo', 'ERRO 02: Símbolo "BOOLEAN" inesperado. Esperando BEGIN - Linha 2, Coluna 3.'],
+      ['erro2_bloco', 'ERRO 02: Símbolo "END" inesperado. Esperando ";" - Linha 5, Coluna 1.'],
+      ['erro2_condicional', 'ERRO 02: Símbolo "X" inesperado. Esperando "(" - Linha 4, Coluna 6.'],
+      ['erro2_comando_all', 'ERRO 02: Símbolo "2" inesperado. Esperando ID - Linha 4, Coluna 7.'],
+      ['erro2_atribuicao', 'ERRO 02: Símbolo "=" inesperado. Esperando ":=" - Linha 4, Coluna 5.'],
+      ['erro2_itera_while', 'ERRO 02: Símbolo "X" inesperado. Esperando "(" - Linha 4, Coluna 9.'],
+      ['erro2_itera_repeat', 'ERRO 02: Símbolo "0" inesperado. Esperando "(" - Linha 4, Coluna 16.'],
+      ['erro2_multi_op_relacional', 'ERRO 02: Símbolo "NOT" inesperado. Esperando ")" - Linha 4, Coluna 15.'],
+      ['erro2_op_relacional', 'ERRO 02: Símbolo "+" inesperado. Esperando OP_RELACIONAL - Linha 4, Coluna 9.'],
+      ['erro2_op_arit', 'ERRO 02: Símbolo "-" inesperado. Esperando ID ou INTEGER ou REAL - Linha 4, Coluna 12.'],
+      ['erro2_multi_arit', 'ERRO 02: Símbolo "3" inesperado. Esperando "(" - Linha 4, Coluna 18.'],
+    ]
+
+    files.each do |file_name, message|
+      arquivo = File.read("spec/algoritmos/erros/parser/#{file_name}")
+      $scan = Scanner.new(arquivo).scan
+      begin
+        Parser.new.parse
+      rescue StandardError => e
+        expect(e.class).to eq(ParserError)
+        expect(e.message).to eq(message)
+      end
+    end
+  end
+end
--- spec/scanner_spec.rb
+++ spec/scanner_spec.rb
@@ -3,12 +3,43 @@ require '../lib/token'
 
 RSpec.describe Scanner do
   it 'Lê os tokens em um arquivo de entrada' do
-    arquivo = File.read('spec/data/algoritmo1')
+    arquivo = File.read('spec/algoritmos/algoritmo1')
     scan = Scanner.new(arquivo).scan
     ignorados = [:ABRE_CHAVE, :FECHA_CHAVE, :FIM]
     expected_types = Token.types.keys.sort - ignorados
     types = scan.todos_tokens.map(&:type).uniq.sort - ignorados
+    File.delete(Scanner::ARQUIVO_SAIDA)
 
     expect(types).to eq(expected_types)
+  end
+
+  it 'Detecta um token inválido' do
+    arquivo = File.read('spec/algoritmos/erros/scanner/erro1')
+    begin
+      Scanner.new(arquivo).scan
+    rescue StandardError => e
+      expect(e.class).to eq(ScannerError)
+      expect(e.message).to eq('ERRO 01: Identificador ou símbolo inválido. "[" - Linha 5, Coluna 9.')
+    end
+  end
+
+  it 'Ignora comentários' do
+    tipos_validos = ['tipo1', 'tipo2', 'tipo3']
+    tipos_validos.each do |file_name|
+      arquivo = File.read("spec/algoritmos/comentarios/#{file_name}")
+      scan = Scanner.new(arquivo).scan
+
+      expect(scan.todos_tokens.count).to eq(10)
+    end
+
+    arquivo = File.read('spec/algoritmos/erros/scanner/comentario')
+    begin
+      Scanner.new(arquivo).scan
+    rescue StandardError => e
+      expect(e.class).to eq(ScannerError)
+      expect(e.message).to eq('ERRO 01: Identificador ou símbolo inválido. "APÓS" - Linha 8, Coluna 39.')
+    end
+
+    File.delete(Scanner::ARQUIVO_SAIDA)
   end
 end
