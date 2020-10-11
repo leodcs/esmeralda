b2dfa862addfe5865cd832399cb7c6130c941bdc
Leonardo Santos <aleotory@gmail.com>
2020-10-11 13:07:01 -0300

    Adiciona método "vazio?" a todos os objetos

--- lib/config/object.rb
+++ lib/config/object.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 class Object
-  def blank?
+  # Método personalizado para verificar se um objeto está vazio ou não
+  # Qualquer tipo de objeto vai extender esse método através de herança
+  # Exemplos
+  # "".vazio? => true
+  # "texto".vazio? => false
+  # [].vazio? => true
+  # [nil].vazio? => false
+  # 0.vazio? => false
+  # nil.vazio? => true
+  def vazio?
     respond_to?(:empty?) ? !!empty? : !self
-  end
-
-  def present?
-    !blank?
   end
 end
